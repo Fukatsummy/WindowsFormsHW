bfde3333f753a2a394267c6c2b7c70b10c51c474
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-11-06 16:16:15 +0300

    Add project files.

---
+++ WFHW/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WFHW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Message = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Message
+            // 
+            this.Message.Location = new System.Drawing.Point(175, 155);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(100, 20);
+            this.Message.TabIndex = 0;
+            this.Message.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Message);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Message;
+    }
+}
+
---
+++ WFHW/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Вывести на экран свое(краткое!!!) резюме с помощью последовательности MessageBox’ов(числом не менее трех). Причем на заголовке
+//последнего должно отобразиться среднее число символов на странице (общее количество символов в резюме / количество MessageBox’ов).
+namespace WFHW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string[] arr = { "Студент: Лукьянчикова Анастасия", "Предмет: Windows Forms", "Группа: ПВ-111" };
+            int element = 0;
+            string caption;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                element += arr[i].Length;
+                caption = (arr.Length - 1 == i) ? $"MessageBox {i + 1}. Среднее число символов - {element / arr.Length}" : $"MessageBox {i + 1}";
+                MessageBox.Show(arr[i], caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}

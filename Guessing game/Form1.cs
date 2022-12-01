2f148fc553668b4f82c2efff4704fbc8b41c8a9e
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-12-01 21:47:18 +0300

    Guessing_game

--- Guessing game/Form1.cs
+++ Guessing game/Form1.cs
@@ -10,11 +10,37 @@ using System.Windows.Forms;
 
 namespace Guessing_game
 {
+
+    //Написать функцию, которая «угадывает» задуманное пользователем число от 1 до 2000. 
+    //Для запроса к пользователю использовать MessageBox.После того, как число отгадано, 
+    //необходимо вывести количество запросов, потребовавшихся для этого, и предоставить
+    //пользователю возможность сыграть еще раз, не выходя из программы 
+    //(MessageBox’ы оформляются кнопками и значками соответственно ситуации).
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            this.Shown += StartGame;
+        }
+
+        private void StartGame(object sender, EventArgs e)
+        {
+            DialogResult result;
+            int numDialog = 1;
+            while (true)
+            {
+                result = MessageBox.Show($"{new Random().Next(1, 2000)}", "Вы загадали число", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    MessageBox.Show($"Количество запросов {numDialog}", "Ура!!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    numDialog = 0;
+                    result = MessageBox.Show($"Хотите продолжить?", "Выход", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (result == DialogResult.No) this.Close();
+                }
+                numDialog++;
+            }
         }
     }
+
 }
